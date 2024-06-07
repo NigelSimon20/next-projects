2cd7f9a25e5485801b919be7b445986413324938
nigel <nigel@uncommon.org>
2024-06-07 15:37:52 +0200

    card-footer

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import Button from './button'
-import CardFooter from './cardfooter'
+import CardFooter from './cardFooter'
 import laurasmith from '../../public/images/laurasmith.png'
 
 interface display{
@@ -11,7 +11,7 @@ interface display{
 
 const Card = ({style}: display) => {
   return (
-    <div className='bg-[#1A1B21] w-[30%] rounded-md text-white'>
+    <div className='bg-[#1A1B21] w-[30%] text-white rounded-lg mb-5'>
         <Image src={laurasmith} alt="smith" className='object-cover w-[100%] mb-6' />
         <div className='px-10'>
         <h1 className='text-center mb-2 md:font-bold text-2xl'>Laura Smith</h1>
@@ -19,9 +19,9 @@ const Card = ({style}: display) => {
         <Link href='/' className='text-center mb-6 block text-sm'>laurasmith.website</Link>
 
         <div className="flex space-x-9 justify-center mb-5">
-        <Button text='Email' bgColor='black' color='black' />
+        <Button text='Email' bgColor='black' color='white' />
          <div className={`${style}`}>
-         <Button text='LinkedIn' bgColor='orange-500' color='black' />
+         <Button text='LinkedIn' bgColor='black' color='black' />
          </div>
         </div>
 
@@ -30,7 +30,7 @@ const Card = ({style}: display) => {
         <h2 className='text-lg md:font-bold mb-3'>Interest</h2>
         <p className='text-sm'>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
         </div>
-        <CardFooter />
+        <CardFooter display='hidden'/>
     </div>
   )
 }
--- app/components/cardFooter.tsx
+++ app/components/cardFooter.tsx
@@ -4,14 +4,31 @@ import {
     FaFacebookF,
     FaInstagram,
     FaGithub,
-    FaLinkedinIn
+    FaLinkedinIn,
+    FaLinkedin
     } from 'react-icons/fa6'
 
-const CardFooter = () => {
+    interface linkedDisplay{
+        display: string;
+    }
+
+const CardFooter = ({display}: linkedDisplay) => {
   return (
-    <div className='flex bg-[#161617] py-16'>
-        <div className='bg-[#918E9B] h-10 w-10'>
-            <FaTwitter className='text-[40px]'/>
+    <div className='flex bg-[#161617] py-10 items-center justify-center space-x-4 mt-10'>
+        <div className='bg-[#918E9B] h-10 w-10 flex items-center justify-center text-[#161617] rounded-lg'>
+            <FaTwitter className='text-[25px]'/>
+        </div>
+        <div className='bg-[#918E9B] h-10 w-10 flex items-center justify-center text-[#161617] rounded-lg'>
+            <FaFacebookF className='text-[25px]'/>
+        </div>
+        <div className='bg-[#918E9B] h-10 w-10 flex items-center justify-center text-[#161617] rounded-lg'>
+            <FaInstagram className='text-[25px]'/>
+        </div>
+        <div className='bg-[#918E9B] h-10 w-10 flex items-center justify-center text-[#161617] rounded-lg'>
+            <FaGithub className='text-[25px]'/>
+        </div>
+        <div className={`bg-[#918E9B] h-10 w-10 flex items-center justify-center text-[#161617] rounded-lg ${display}`}>
+            <FaLinkedinIn className='text-[25px]'/>
         </div>
     </div>
   )
